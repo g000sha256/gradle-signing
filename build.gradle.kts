b10a117edd17849be1abfb515bbfebcda2cb3fc5
Georgii Ippolitov <github@g000sha256.dev>
2026-05-09 21:36:40 +0300

    Version 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
 
 group = "dev.g000sha256"
-version = "1.0.0"
+version = "1.0.1"
 
 plugins {
     alias(notation = catalog.plugins.g000sha256.sonatypeMavenCentral)
